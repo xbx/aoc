806a3d68f26a52b60dd7d5e68ba700b319e134b5
xbx <xbx.roberto@gmail.com>
2023-12-05 19:16:22 +0000

    d04-1

--- day-03/src/part2.rs
+++ day-03/src/part2.rs
@@ -64,15 +64,31 @@ fn parse_line(previous_line: &str, current_line: &str, next_line: &str) -> isize
     for char in chars {
 
         if is_symbol(&char) {
-            let prev_number = get_adjacent_number(previous_line, current_pos);
-            let curr_number = get_adjacent_number(current_line, current_pos);
-            let next_number = get_adjacent_number(next_line, current_pos);
+            let (prev_number_l, prev_number_r) = get_adjacent_number(previous_line, current_pos);
+            let (curr_number_l, curr_number_r) = get_adjacent_number(current_line, current_pos);
+            let (next_number_l, next_number_r) = get_adjacent_number(next_line, current_pos);
 
-            if (prev_number != 1 && curr_number != 1)
-                || (prev_number != 1 && next_number != 1)
-                || (curr_number != 1 && next_number != 1)
-            {
-                total += prev_number * curr_number * next_number;
+            let mut total_parts = 0;
+            if prev_number_l > 1 {
+                total_parts += 1;
+            }
+            if prev_number_r > 1 {
+                total_parts += 1;
+            }
+            if curr_number_l > 1 {
+                total_parts += 1;
+            }
+            if curr_number_r > 1 {
+                total_parts += 1;
+            }
+            if next_number_l > 1 {
+                total_parts += 1;
+            }
+            if next_number_r > 1 {
+                total_parts += 1;
+            }
+            if total_parts == 2 {
+                total += prev_number_l * prev_number_r * curr_number_l * curr_number_r * next_number_l * next_number_r;
             }
         }
         current_pos += 1;
@@ -84,70 +100,77 @@ fn parse_line(previous_line: &str, current_line: &str, next_line: &str) -> isize
 fn get_adjacent_number(
     line: &str,
     current_pos: usize,
-) -> isize {
+) -> (isize, isize) {
     let line_vec: Vec<_> = line.chars().collect();
+    let mut left_number = 1;
+    let mut right_number = 1;
 
     if !is_digit(&line_vec[current_pos - 1])
         && !is_digit(&line_vec[current_pos])
         && !is_digit(&line_vec[current_pos + 1]) {
-        return 1
+        return (1, 1)
     }
 
-    let mut left_most = current_pos;
-    let mut right_most = current_pos;
+    let mut left_most: usize;
+    let mut right_most: usize;
     
     if is_digit(&line_vec[current_pos]) {
-        left_most = current_pos;
+        left_most = loop_left_most(&line_vec, current_pos);
+        right_most = loop_right_most(&line_vec, current_pos);
+        right_number = parse_number(&line[left_most..right_most]);
+    } else if is_digit(&line_vec[current_pos - 1]) && is_digit(&line_vec[current_pos + 1]) {
+        // get from the left
         right_most = current_pos;
-        loop {
-            if left_most == 0 { break }
-            if !is_digit(&line_vec[left_most]) {
-                break;
-            } else {
-                left_most -= 1;
-            }
-        }
-        loop {
-            if right_most == line_vec.len() {
-                right_most = line_vec.len() - 1;
-                break
-            }
-            if !is_digit(&line_vec[right_most]) {
-                break;
-            } else {
-                right_most += 1;
-            }
-        }
+        left_most = loop_left_most(&line_vec, current_pos - 1);
+        left_number = parse_number(&line[left_most..right_most]);
+
+        // get from the right
+        left_most = current_pos + 1;
+        right_most = loop_right_most(&line_vec, current_pos + 1);
+        right_number = parse_number(&line[left_most..right_most]);
+
     } else if is_digit(&line_vec[current_pos - 1]) {
         right_most = current_pos;
-        left_most = current_pos - 1;
-        loop {
-            if left_most == 0 { break }
-            if !is_digit(&line_vec[left_most]) {
-                break;
-            } else {
-                left_most -= 1;
-            }
-        }
+        left_most = loop_left_most(&line_vec, current_pos - 1);
+        left_number = parse_number(&line[left_most..right_most]);
     } else if is_digit(&line_vec[current_pos + 1]) {
         left_most = current_pos + 1;
-        right_most = current_pos + 1;
-        loop {
-            if right_most == line_vec.len() {
-                right_most = line_vec.len() - 1;
-                break
-            }
-            if !is_digit(&line_vec[right_most]) {
-                break;
-            } else {
-                right_most += 1;
-            }
+        right_most = loop_right_most(&line_vec, current_pos + 1);
+        right_number = parse_number(&line[left_most..right_most]);
+    }
+
+    (left_number, right_number)
+}
+
+fn loop_left_most(line: &Vec<char>, current_pos: usize) -> usize {
+    let mut left_most = current_pos;
+    loop {
+        if left_most == 0 { break }
+        if !is_digit(&line[left_most]) {
+            break;
+        } else {
+            left_most -= 1;
         }
     }
 
-    let number_slice = &line[left_most..right_most];
+    left_most
+}
 
-    parse_number(number_slice)
+fn loop_right_most(line: &Vec<char>, current_pos: usize) -> usize {
+    let mut right_most = current_pos;
+    loop {
+        if right_most == line.len() {
+            right_most = line.len() - 1;
+            break
+        }
+        if !is_digit(&line[right_most]) {
+            break;
+        } else {
+            right_most += 1;
+        }
+    }
+
+    right_most
 }
 
 
---
+++ day-04/src/part1.rs
@@ -0,0 +1,93 @@
+use crate::custom_error::AocError;
+use std::collections::HashSet;
+use regex::Regex;
+
+#[tracing::instrument]
+pub fn process(
+    input: &str,
+) -> miette::Result<String, AocError> {
+
+    let mut total = 0;
+    
+    let lines: Vec<_> = input.split("\n").collect();
+    
+    for i in 0..(lines.len()) {
+        total += parse_line(&lines[i]);
+    }
+
+    Ok(String::from(total.to_string()))
+}
+
+fn parse_line(line: &str) -> isize {
+    let numbers = get_numbers(line);
+    let winning = numbers.0;
+    let have = numbers.1;
+    
+    let mut points = 0;
+    for winner in winning {
+        let won = have.get(&winner);
+        match won {
+            None => continue,
+            Some(_) => {
+                if points == 0 {
+                    points = 1;
+                } else {
+                    points = points * 2;
+                }
+            }
+        }
+    }
+
+    points
+}
+
+fn get_numbers(line: &str) -> (HashSet<isize>, HashSet<isize>) {
+    let re: Regex = Regex::new(" +").expect("regex err");
+    let card_info: Vec<_> = line.split(": ").collect();
+    let both_sides: Vec<_> = card_info[1].split(" | ").collect();
+
+    let winning_iter = re.split(both_sides[0]);
+    let winning_numbers = winning_iter.map(|num| parse_number(num));
+
+    let have_iter = re.split(both_sides[1]);
+    let have_numbers = have_iter.map(|num| parse_number(num));
+
+    let winning: HashSet<isize> = HashSet::from_iter(winning_numbers);
+    let have: HashSet<isize> = HashSet::from_iter(have_numbers);
+
+    (winning, have)
+}
+
+fn parse_number(number_str: &str) -> isize {
+    let number_str_trim = number_str.trim();
+    if number_str_trim.len() > 0 {
+        number_str_trim.parse::<isize>().unwrap()
+    } else {
+        -1
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+";
+        let output = process(input.trim());
+        assert_eq!(13.to_string(), output?);
+
+        Ok(())
+    }
+
+}
+
+
